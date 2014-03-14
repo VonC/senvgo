78240c5399c4fcd4bc2958811ccd236be8ed7704
VonC <vonc@laposte.net>
2014-03-14 22:15:06 +0100

    Trivial install function.

--- main.go
+++ main.go
@@ -1,7 +1,68 @@
 package main
 
-import "runtime"
+import (
+	"fmt"
+	"os"
+	"runtime"
+)
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
+	prgnames := ""
+	prgs := ResolveDependencies([]string{prgnames})
+	for _, prg := range prgs {
+		install(prg)
+	}
+}
+
+type Prg struct {
+	name       string
+	extractVer *Extractor
+	extractUrl *Extractor
+}
+
+type fextract func(str string) (string, fextract)
+
+type Extractor struct {
+	extract fextract
+}
+
+func ResolveDependencies(prgnames []string) []*Prg {
+	prgPeazip := &Prg{name: "peazip"}
+	prgGit := &Prg{name: "git"}
+	prgInvalid := &Prg{name: "invalid"}
+	return []*Prg{prgPeazip, prgGit, prgInvalid}
+}
+
+func install(prg *Prg) {
+	folder := prg.GetFolder()
+	if hasFolder, err := exists(folder); !hasFolder && err == nil {
+		fmt.Printf("Need to install %v in '%v'\n", prg.name, folder)
+	}
+}
+
+func (prg *Prg) GetFolder() string {
+	folder := ""
+	switch prg.name {
+	case "peazip":
+		folder = "pz5.2.2"
+	case "git":
+		folder = "git1.9"
+	case "invalid":
+		folder = "invalid<x:y"
+	}
+	return "test/" + prg.name + "/" + folder
+}
+
+// exists returns whether the given file or directory exists or not
+// http://stackoverflow.com/questions/10510691/how-to-check-whether-a-file-or-directory-denoted-by-a-path-exists-in-golang
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
 }
