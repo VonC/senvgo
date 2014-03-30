21b6d263be15802af7fd36b71cdf0a696742eadd
VonC <vonc@laposte.net>
2014-03-30 14:14:30 +0200

    CacheDisk#Get() uses itself to get its own folder.

--- main.go
+++ main.go
@@ -222,7 +222,7 @@ func (c *CacheDisk) Get(resource string, name string, isArchive bool) string {
 	if c.last == "" {
 		sha := c.getResourceName(resource, name, isArchive)
 		t := time.Now()
-		filename := cache.Folder(name) + name + "_" + sha + "_" + t.Format("20060102") + "_" + t.Format("150405")
+		filename := c.Folder(name) + name + "_" + sha + "_" + t.Format("20060102") + "_" + t.Format("150405")
 		fmt.Printf("Get '%v' downloads '%v' for '%v'\n", c.id, filename, resource)
 		c.last = download(resource, filename, true)
 		fmt.Printf("Get '%v' has downloaded '%v' bytes in '%v' for '%v'\n", c.id, len(c.last), filename, resource)
