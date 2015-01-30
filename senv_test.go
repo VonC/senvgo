0d7cd98e9f614d5e6a59f743c499b79c45ea620c
VonC <vonc@laposte.net>
2015-01-30 10:35:56 +0100

    Reset implementation (see main.old)

--- senv_test.go
+++ senv_test.go
@@ -3,12 +3,18 @@ package main
 import (
 	"testing"
 
+	. "github.com/VonC/godbg"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestMain(t *testing.T) {
 
 	Convey("senvgo call be called", t, func() {
+		godbg.SetBuffers(nil)
 		main()
+		So(OutString(), ShouldEqual, ``)
+		So(ErrString(), ShouldEqual, `  [main:7] (func.001:14)
+    senvgo
+`)
 	})
 }
