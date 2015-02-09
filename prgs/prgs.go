12e7913d3c1deaeae1fa76fb0897af48ea4b947b
VonC <vonc@laposte.net>
2015-02-09 15:30:49 +0000

    Make Prg an interface instead of a struct

--- prgs/prgs.go
+++ prgs/prgs.go
@@ -1,11 +1,19 @@
 package prgs
 
 // Prg is a Program, with all its data (no behavior)
-type Prg struct{}
+type prg struct {
+	name string
+}
+
+// Prg defines what kind of service a program has to provide
+type Prg interface {
+	// Name is the name of a program to install, acts as an id
+	Name() string
+}
 
 // PGetter gets programs (from an internal config)
 type PGetter interface {
-	Get() []*Prg
+	Get() []Prg
 }
 
 type defaultGetter struct{}
@@ -17,10 +25,15 @@ func init() {
 	dg = defaultGetter{}
 	getter = dg
 }
-func (df defaultGetter) Get() []*Prg {
-	return []*Prg{}
+func (df defaultGetter) Get() []Prg {
+	return []Prg{}
 }
 
+// Getter returns a object able to get a list of Prgs
 func Getter() PGetter {
 	return getter
+}
+
+func (p *prg) Name() string {
+	return p.name
 }
