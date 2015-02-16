1e8cb6eac9b77fef11d421fa66eceaaf706223de
VonC <vonc@laposte.net>
2015-02-16 11:05:57 +0100

    Add an installer to senv main function

--- senv.go
+++ senv.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/VonC/godbg"
 	"github.com/VonC/godbg/exit"
+	"github.com/VonC/senvgo/installer"
 	"github.com/VonC/senvgo/prgs"
 )
 
@@ -12,9 +13,14 @@ var exiter *exit.Exit
 var status int
 var prgsGetter prgs.PGetter
 
+type newInstallerFunc func(prgs.Prg) installer.Inst
+
+var newInstaller newInstallerFunc
+
 func init() {
 	exiter = exit.Default()
 	prgsGetter = prgs.Getter()
+	newInstaller = installer.New
 }
 
 func main() {
@@ -32,8 +38,13 @@ func run() int {
 		return 0
 	}
 	for i, prg := range prgs {
+		inst := newInstaller(prg)
 		fmt.Fprintf(godbg.Out(), "'%s' (%d/%d)... ", prg.Name(), i+1, nbprgs)
-		fmt.Fprintf(godbg.Out(), "already installed: nothing to do\n")
+		if inst.IsInstalled() {
+			fmt.Fprintf(godbg.Out(), "already installed: nothing to do\n")
+		} else if inst.HasFailed() {
+			fmt.Fprintf(godbg.Out(), "already failed to install\n")
+		}
 	}
 	return 0
 }
