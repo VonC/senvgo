da258996879ef87d03a4d7ee9a531afd5d580879
VonC <vonc@laposte.net>
2015-02-18 17:25:05 +0100

    Start testing PathWriter

--- paths/paths.go
+++ paths/paths.go
@@ -10,5 +10,22 @@ import (
 type PathWriter interface {
 	// WritePath writes in a writer `set PATH=`... with all prgs PATH.
 	// Note: not all programs have a path
-	WritePath(prgs []prgs.Prg, w io.Writer)
+	WritePath(prgs []prgs.Prg, w io.Writer) error
+}
+
+type pathWriter struct{}
+
+func (pw *pathWriter) WritePath(prgs []prgs.Prg, w io.Writer) error {
+	for _, prg := range prgs {
+		if _, err := w.Write([]byte(prg.Name())); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+var pw *pathWriter
+
+func init() {
+	pw = &pathWriter{}
 }
--- paths/paths_test.go
+++ paths/paths_test.go
@@ -1,6 +1,7 @@
 package paths
 
 import (
+	"bytes"
 	"io"
 	"testing"
 
@@ -9,9 +10,13 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-type testPathWriter struct{}
+type testPathWriter struct{ b bytes.Buffer }
 
-func (tpw *testPathWriter) WritePath(prgs []prgs.Prg, w io.Writer) {
+func (tpw *testPathWriter) WritePath(prgs []prgs.Prg, w io.Writer) error {
+	if err := pw.WritePath(prgs, w); err != nil {
+		return err
+	}
+	return nil
 }
 func TestMain(t *testing.T) {
 	tpw := &testPathWriter{}
@@ -19,7 +24,9 @@ func TestMain(t *testing.T) {
 	Convey("A Path writer writes any empty path if no prgs", t, func() {
 		SetBuffers(nil)
 		prgs := []prgs.Prg{}
-		tpw.WritePath(prgs, nil)
+		err := tpw.WritePath(prgs, &tpw.b)
+		So(err, ShouldBeNil)
+		So(tpw.b.String(), ShouldEqual, "e")
 	})
 
 }
