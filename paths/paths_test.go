737fa1114c90cc5c1fd0bb937f14c396a09d9e62
VonC <vonc@laposte.net>
2015-02-19 09:39:26 +0100

    Test PathWriter error case

--- paths/paths_test.go
+++ paths/paths_test.go
@@ -2,6 +2,7 @@ package paths
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"testing"
 
@@ -10,7 +11,21 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-type testPathWriter struct{ b bytes.Buffer }
+type testPrg struct{ name string }
+
+func (tp *testPrg) Name() string { return tp.name }
+
+type testPathWriter struct{ b *bytes.Buffer }
+
+type testWriter struct{ w io.Writer }
+
+func (tw *testWriter) Write(p []byte) (n int, err error) {
+	s := string(p)
+	if s == "prg2" {
+		return 0, fmt.Errorf("Error writing '%s'", s)
+	}
+	return tw.w.Write(p)
+}
 
 func (tpw *testPathWriter) WritePath(prgs []prgs.Prg, w io.Writer) error {
 	if err := pw.WritePath(prgs, w); err != nil {
@@ -18,15 +33,24 @@ func (tpw *testPathWriter) WritePath(prgs []prgs.Prg, w io.Writer) error {
 	}
 	return nil
 }
+
 func TestMain(t *testing.T) {
-	tpw := &testPathWriter{}
+	tpw := &testPathWriter{b: bytes.NewBuffer(nil)}
+	prgs := []prgs.Prg{&testPrg{name: "prg1"}, &testPrg{name: "prg2"}}
 
 	Convey("A Path writer writes any empty path if no prgs", t, func() {
 		SetBuffers(nil)
-		prgs := []prgs.Prg{}
-		err := tpw.WritePath(prgs, &tpw.b)
+		err := tpw.WritePath(prgs, tpw.b)
 		So(err, ShouldBeNil)
-		So(tpw.b.String(), ShouldEqual, "e")
+		So(tpw.b.String(), ShouldEqual, "prg1prg2")
 	})
 
+	Convey("A Path writer can report error during writing", t, func() {
+		SetBuffers(nil)
+		tpw.b = bytes.NewBuffer(nil)
+		tw := &testWriter{w: tpw.b}
+		err := tpw.WritePath(prgs, tw)
+		So(err.Error(), ShouldEqual, "Error writing 'prg2'")
+		So(tpw.b.String(), ShouldEqual, "prg1")
+	})
 }
