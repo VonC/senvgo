e9296f30c2ef3f83d5376eb468dc7c671cc289a0
VonC <vonc@laposte.net>
2015-02-19 09:42:29 +0100

    [golint] fix comment on PathWriter

--- paths/paths.go
+++ paths/paths.go
@@ -6,7 +6,7 @@ import (
 	"github.com/VonC/senvgo/prgs"
 )
 
-// Compute final PATH of a collection of programs
+// PathWriter computes final PATH of a collection of programs
 type PathWriter interface {
 	// WritePath writes in a writer `set PATH=`... with all prgs PATH.
 	// Note: not all programs have a path
