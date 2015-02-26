8b792e3601e79d7d56abc4be695c22fb16483089
VonC <vonc@laposte.net>
2015-02-26 21:57:03 +0100

    Add and test envs Prgsenv() for PRGS value

--- envs/envs.go
+++ envs/envs.go
@@ -1,14 +1,18 @@
 package envs
 
 import (
+	"fmt"
 	"os"
 	"strings"
+
+	"github.com/VonC/godbg"
+	"github.com/VonC/senvgo/paths"
 )
 
 type EnvGetter func(key string) string
 
 var envGetterFunc EnvGetter
-var paths []string
+var pathsegments []string
 
 func init() {
 	envGetterFunc = os.Getenv
@@ -17,9 +21,30 @@ func init() {
 // PathSegments returns the environment variable PATH split per segment.
 // Each segment is a path initially separated by a ';'
 func PathSegments() []string {
-	if paths == nil {
+	if pathsegments == nil {
 		p := envGetterFunc("PATH")
-		paths = strings.Split(p, ";")
+		pathsegments = strings.Split(p, ";")
 	}
-	return paths
+	return pathsegments
+}
+
+var _prgsenv *paths.Path
+var _prgsenvname = "PRGS2"
+
+// Prgsenv() checks if %PRG% is defined.
+// Panics otherwise. Cache the value if defined.
+func Prgsenv() *paths.Path {
+	if _prgsenv != nil {
+		return _prgsenv
+	}
+	prgse := envGetterFunc(_prgsenvname)
+	if prgse == "" {
+		err := fmt.Errorf("no env variable '%s' defined", _prgsenvname)
+		godbg.Pdbgf(err.Error())
+		panic(err)
+	} else {
+		_prgsenv = paths.NewPathDir(prgse)
+		godbg.Pdbgf("PRGS2='%v'", _prgsenv)
+	}
+	return _prgsenv
 }
--- envs/envs_test.go
+++ envs/envs_test.go
@@ -1,9 +1,12 @@
 package envs
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
 	. "github.com/VonC/godbg"
+	"github.com/VonC/senvgo/paths"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
@@ -18,6 +21,77 @@ func TestMain(t *testing.T) {
 		envGetterFunc = testEnvGetter
 		paths := PathSegments()
 		So(len(paths), ShouldEqual, 2)
+		So(NoOutput(), ShouldBeTrue)
+		envGetterFunc = os.Getenv
 	})
 
+}
+
+func TestPrgsEnv(t *testing.T) {
+
+	Convey("Envs can returns PRGS environment variable", t, func() {
+
+		Convey("Non-set PRGS env variable means panic", func() {
+			fmt.Println("ok!")
+			SetBuffers(nil)
+			envGetterFunc = testPrgsEnvGetter
+			prgsenvtest = "notset"
+			var p *paths.Path
+			defer func() {
+				recover()
+				So(p, ShouldBeNil)
+				So(OutString(), ShouldBeEmpty)
+				So(ErrString(), ShouldEqualNL, `  [Prgsenv:47] (func.003:49)
+    no env variable 'PRGS2' defined`)
+				envGetterFunc = os.Getenv
+				prgsenvtest = ""
+			}()
+			p = Prgsenv()
+		})
+
+		Convey("Set PRGS env variable means Path dir", func() {
+			fmt.Println("ok!")
+			SetBuffers(nil)
+			envGetterFunc = testPrgsEnvGetter
+			prgsenvtest = "set"
+			p := Prgsenv()
+			So(p, ShouldNotBeNil)
+			So(p.String(), ShouldEqual, `set\`)
+			So(OutString(), ShouldBeEmpty)
+			So(ErrString(), ShouldEqualNL, `  [Prgsenv:52] (func.004:57)
+    PRGS2='set\'`)
+			envGetterFunc = os.Getenv
+			prgsenvtest = ""
+		})
+
+		Convey("Set PRGS env get twice means cached", func() {
+			fmt.Println("ok!")
+			SetBuffers(nil)
+			_prgsenv = nil
+			envGetterFunc = testPrgsEnvGetter
+			prgsenvtest = "set"
+			So(_prgsenv, ShouldBeNil)
+			p := Prgsenv()
+			So(p, ShouldNotBeNil)
+			So(p.String(), ShouldEqual, `set\`)
+			p1 := Prgsenv()
+			So(p, ShouldEqual, p1)
+			envGetterFunc = os.Getenv
+			prgsenvtest = ""
+		})
+
+	})
+
+}
+
+var prgsenvtest = ""
+
+func testPrgsEnvGetter(key string) string {
+	if prgsenvtest == "notset" {
+		return ""
+	}
+	if prgsenvtest == "set" {
+		return "set"
+	}
+	return "tbd"
 }
