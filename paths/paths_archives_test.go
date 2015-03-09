f95478f1a3f2f30970060e505fd4f2850f0ce2cd
VonC <vonc@laposte.net>
2015-03-09 20:51:37 +0100

    Add testzip.zip for testing zip uncompress

--- paths/paths_archives_test.go
+++ paths/paths_archives_test.go
@@ -34,3 +34,20 @@ func TestArchive(t *testing.T) {
 		})
 	})
 }
+
+/*
+C:\Users\vonc\prog\go\src\github.com\VonC\senvgo\paths>mkdir testzip
+C:\Users\vonc\prog\go\src\github.com\VonC\senvgo\paths>echo a> testzip\a.txt
+C:\Users\vonc\prog\go\src\github.com\VonC\senvgo\paths>echo ab> testzip\b.txt
+C:\Users\vonc\prog\go\src\github.com\VonC\senvgo\paths>mkdir testzip\c
+C:\Users\vonc\prog\go\src\github.com\VonC\senvgo\paths>echo abcd> testzip\c\abcd.txt
+http://askubuntu.com/questions/58889/how-can-i-create-a-zip-archive-of-a-whole-directory-via-terminal-without-hidden
+C:\Users\vonc\prog\go\src\github.com\VonC\senvgo\paths>zip -r testzip.zip testzip
+  adding: testzip/ (164 bytes security) (stored 0%)
+  adding: testzip/a.txt (164 bytes security) (stored 0%)
+  adding: testzip/b.txt (164 bytes security) (stored 0%)
+  adding: testzip/c/ (164 bytes security) (stored 0%)
+  adding: testzip/c/abcd.txt (164 bytes security) (stored 0%)
+C:\Users\vonc\prog\go\src\github.com\VonC\senvgo\paths>testzip.zip
+C:\Users\vonc\prog\go\src\github.com\VonC\senvgo\paths>rm -Rf testzip
+*/
