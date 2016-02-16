bdc7fd8deba75fdb0031fc1358e07cd665f20c09
VonC <vonc@laposte.net>
2016-02-16 14:59:18 +0100

    First empty test case in inst

--- gb.bat
+++ gb.bat
@@ -1,4 +1,5 @@
 @echo off
 rem @setlocal enableextensions enabledelayedexpansion
 call %~dp0senv.bat
+go test github.com/VonC/senvgo/inst
 endlocal
---
+++ inst/inst_test.go
@@ -0,0 +1,6 @@
+package inst
+
+import "testing"
+
+func TestCheckInst(t *testing.T) {
+}
